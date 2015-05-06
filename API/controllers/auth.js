c26a7f8068c98b2140983e55087993f01309b69c
valentinLecorps <valentin.lecorps@gmail.com>
2015-05-06 21:58:19 +0200

    - Test to return url after auth

--- API/controllers/auth.js
+++ API/controllers/auth.js
@@ -25,8 +25,7 @@
  		return res.json({"success": true});
  	});
 
- 	app.get('/auth/facebook',
- 		modules.passport.authenticate('facebook',{ authType: 'rerequest', scope: ['email','user_birthday'] }));
+ 	app.get('/auth/facebook', modules.passport.authenticate('facebook',{ authType: 'rerequest', scope: ['email','user_birthday'] }));
 
  // 	app.get('/auth/facebook/callback',
  // 		modules.passport.authenticate('facebook', { failureRedirect: '/login' }),
@@ -37,9 +36,9 @@
  // 				console.log(sessions);
  // 				return res.redirect('/#/auth/success');
  // 			});
-	// });
+ //		});
 
-	 	app.get('/auth/facebook/callback', function(req, res, next) {
+	app.get('/auth/facebook/callback', function(req, res, next) {
  		modules.passport.authenticate('facebook', { failureRedirect: '/login' }, function(err, user) {
  			if(err || !user){ 
  				return res.redirect('/#/auth/error');
@@ -47,8 +46,8 @@
  			req.login(user, function(err) {
  				if(err){ return next(err); }
  				//res.cookie('token', user.token, {expires: new Date(Date.now() + config.ttlToken*1000)});
- 				console.log(sessions);
- 				return res.redirect('/#/auth/success/'+user.token);
+ 				console.log(req);
+ 				return res.redirect(req.headers.referer+'#/auth/success/'+user.token);
  			});
  		})(req, res, next);
  	});
--- API/read_me.txt
+++ API/read_me.txt
@@ -1,3 +1,1 @@
 Change API address, API port and "root_dir" in config.js
-
-Before running any client application you must generate the database. Load <apiAddress>/firstLoad in your web browser. It will create the default plan and the admin user {username: admin, password: admin}
